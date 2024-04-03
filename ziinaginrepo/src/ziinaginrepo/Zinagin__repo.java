7de5224fa084444876cfe3db01a5cdc250823bdf
Admin <Admin@DESKTOP-MDCO2BI>
2024-04-03 11:00:28 +0800

    Erdenebat Zinagin repo

---
+++ ziinaginrepo/src/ziinaginrepo/Zinagin__repo.java
@@ -0,0 +1,10 @@
+package ziinaginrepo;
+
+public class Zinagin__repo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("HIIII Zina");
+	}
+
+}
